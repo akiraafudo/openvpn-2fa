8d29fac8ab3d80135d9844aac1bcf9ef93bbdcc7
akiraafudo <69959725+akiraafudo@users.noreply.github.com>
2024-03-05 17:10:43 +0800

    Update manage.sh

--- manage.sh
+++ manage.sh
@@ -11,17 +11,21 @@ W="\e[0;97m"
 B="\e[1m"
 C="\e[0m"
 
-if [ $# -lt 1 ]; then
+if [ $# -lt 1 ] 
+then 
     echo -e "${W}usage:\n./manage.sh create/revoke <username>\n./manage.sh status\n./manage.sh send <username>${C}"
     exit 1
 fi
 
+
 function emailProfile() {
+
     CLIENT=$1
     PASSWORD=$2
 
+    ### Email the profile to the user
     hostlist=$(cat /etc/hosts | grep -v "#" | grep -v "localhost" | grep -v "127.0.0.1" | grep -v -e "^$")
-
+        
     content="""
 ##########    OpenVPN connection profile (${HOST})  ###################
 
@@ -41,6 +45,7 @@ ${hostlist}
     echo "${content}" | mailx -s "Your OpenVPN profile" -a "${CLIENTDIR}/${CLIENT}/${CLIENT}.ovpn" -a "/opt/openvpn/google-auth/${CLIENT}.png" -r "Devops<devops@company.com>" "${CLIENT}@company.com" || { echo "${R}${B}error mailing profile to client: ${CLIENT}${C}"; exit 1; }
 }
 
+
 function newClient() {
     CLIENT=${1:?}
     CLIENTEXISTS=$(tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep -c -E "/CN=${CLIENT}\$")
@@ -49,21 +54,21 @@ function newClient() {
         exit
     else
         echo -e "${W}new user does not exist, creating..${C}"
-
+        
         # generate user password
         mkdir -p "${CLIENTDIR}/${CLIENT}"
         PW=$(pwgen 15 1)
-        echo "${PW}" >"${CLIENTDIR}/${CLIENT}/pass"
+        echo "${PW}" > "${CLIENTDIR}/${CLIENT}/pass"
 
         cd /etc/openvpn/easy-rsa/ || return
-        echo "${PW}" | ./easyrsa build-client-full "${CLIENT}"
+        echo "${PW}"; echo "${PW}" | ./easyrsa build-client-full "${CLIENT}"
         echo -e "${G}Client $CLIENT added.${C}"
     fi
 
+
     # create system account for new VPN user, add password to it
-    user_exists=$(grep -c "^${CLIENT}:" /etc/passwd)
-    if [ $user_exists -eq 0 ]; then
-        useradd -m -d "${CLIENTDIR}/${CLIENT}" -s /bin/nologin "${CLIENT}" || { echo -e "${R}${B}Error creating system account for ${CLIENT} ${C}"; exit 1; }
+    if ! id "${CLIENT}" &>/dev/null; then
+        useradd -m -d "${CLIENTDIR}/${CLIENT}" -s /bin/nologin "${CLIENT}" || { echo -e "${R}${B}Error creating system account for ${CLIENT} ${C}"; exit 1; }    
     fi
 
     # update system user pw, remove pw expiration
@@ -91,16 +96,19 @@ function newClient() {
     chmod -R 600 "${CLIENTDIR}/${CLIENT}"
 
     echo -e "${W}The configuration file has been written to ${CLIENTDIR}/${CLIENT}/${CLIENT}.ovpn${C}"
+
 }
 
-if [ ! "${ACTION}" == "create" ] && [ ! "${ACTION}" == "revoke" ] && [ ! "${ACTION}" == "status" ] && [ ! "${ACTION}" == "send" ]; then
+if [ ! "${ACTION}" == "create" ] && [ ! "${ACTION}" == "revoke" ] && [ ! "${ACTION}" == "status" ] && [ ! "${ACTION}" == "send" ]
+then
     echo -e "${W}usage:\n./manage.sh create/revoke <username>\n./manage.sh status\n./manage.sh send <username>${C}"
     exit 1
 fi
 
 cd /opt/openvpn || exit 1
 
-if [ "${ACTION}" == "create" ]; then
+if [ "${ACTION}" == "create" ]
+then
     [ -z "${CLIENT}" ] && { echo -e "${R}Provide a username to create${C}"; exit 1; }
 
     newClient "${CLIENT}" || { echo -e "${R}${B}Error generating user VPN profile${C}"; exit 1; }
@@ -114,8 +122,11 @@ if [ "${ACTION}" == "create" ]; then
     emailProfile "${CLIENT}" "${PW}" || { echo -e "${R}${B}Error sending profile to new user ${CLIENT} ${C}"; exit 1; }
 fi
 
-if [ "${ACTION}" == "revoke" ]; then
-    [ -z "${CLIENT}" ] && { echo -e "${R}Provide a username to revoke${C}"; exit 1; }
+
+if [ "${ACTION}" == "revoke" ]
+then
+
+    [ -z "${CLIENT}" ] &&  { echo -e "${R}Provide a username to revoke${C}"; exit 1; }
 
     cd /etc/openvpn/easy-rsa/ || exit 1
 
@@ -127,23 +138,28 @@ if [ "${ACTION}" == "revoke" ]; then
     rm -f /etc/openvpn/crl.pem
     cp /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem
     chmod 644 /etc/openvpn/crl.pem
+    
 
     # remove client from PKI index
     sed -i "/CN=${CLIENT}$/d" /etc/openvpn/easy-rsa/pki/index.txt
 
     # remove user OS acct that was created by OpenVPN manage.sh script
     id "${CLIENT}" && userdel -r -f "${CLIENT}"
-
+    
     rm -rf "${CLIENTDIR:?}/${CLIENT:?}"
 
     echo -e "${G}VPN access for $CLIENT is revoked${C}"
 fi
 
-if [ "${ACTION}" == "status" ]; then
+
+if [ "${ACTION}" == "status" ]
+then
     cat /etc/openvpn/easy-rsa/pki/index.txt | grep "^V" | grep -v "server_"
 fi
 
-if [ "${ACTION}" == "send" ]; then
+if [ "${ACTION}" == "send" ]
+then
+
     [ -z "${CLIENT}" ] && { echo -e "${R}Provide a username to send profile to${C}"; exit 1; }
     PW=$(cat "${CLIENTDIR}/${CLIENT}/pass") || { echo -e "${R}${B}User doesnt exist${C}"; exit 1; }
     emailProfile "${CLIENT}" "${PW}" || { echo -e "${R}${B}Error sending profile to user ${CLIENT}${C}"; exit 1; }
